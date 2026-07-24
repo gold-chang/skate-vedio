a4133e9018a72490fbfebd5a74e81dab77e4137f
goldchang <goldchang.lee@gmail.com>
2026-07-24 19:10:31 +0900

    feat: 스케이트보드 앱 첫 배포 준비 완료

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+mport type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   // 뒤로가기 시 스크롤 위치 자동 복원
   experimental: {
     scrollRestoration: true,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
